9b2fa01a95bf5c5bead3ae5854e68a605b922aef
Amadeo Siles <amadeosilesdx2@gmail.com>
2024-03-27 17:01:39 -0400

    En RegistrarDatosAmigo.jsx al elegir Genero selecciona solo un Radiobutton

--- cliente/src/RegistrarDatosAmigo.jsx
+++ cliente/src/RegistrarDatosAmigo.jsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
 import Navbar from "./Componentes/Navbar";
 function RegistrarDatosAmigo() {
-    return (
 
+  const[Genero, setGenero] = useState("Masculino")
+  const handleChange = (e) => {
+    console.log(e.target.va);
+    setGenero(e.target.value);
+  }
+  return (
       <div>
         <Navbar/>
       <h1>Crear Perfil alqui-amigo</h1>
@@ -9,26 +15,32 @@ function RegistrarDatosAmigo() {
           <h3>Nombre(*)</h3>
           <input type="text" required="" /> 
           <br></br>
+
           <h3>Apellidos(*)</h3>
           <input type="text" required="" />
           <br></br>
+
           <h3>Correo Electronico(*)</h3>
           <input type="email" required="" />
           <br></br>
+
           <h3>Contraseña(*)</h3>
           <input type="password" required=""/>
           <br></br>
+
           <h3>Fecha de nacimiento(*)</h3>
           <input type="date" required="" />
           <br></br>
+
           <h3>Género(*)</h3>
-          <input type="radio" value="Masculino" />
-          <label for="masculino">Masculino</label>
-          <input type="radio" value="Femenino" />
-          <label for="femenino">Femenino</label>
-          <input type="radio" value="Otro" />
-          <label for="otro">Otro</label>
+          <input type="radio" name="Genero" id="Masculino" value="Masculino" onChange={handleChange}/>
+          <label htmlFor="masculino">Masculino</label>
+          <input type="radio" name="Genero" id="Femenino" value="Femenino" onChange={handleChange}/>
+          <label htmlFor="femenino">Femenino</label>
+          <input type="radio" name="Genero" id="Otro" value="Otro" onChange={handleChange}/>
+          <label htmlFor="otro">Otro</label>
           <br></br>
+
           <h3>Elige cuánto que te gustaría obtener por hora(*)</h3>
           <select name="tarifa" id="tarifa">
               <option value="ganar10">Quiero ganar 10Bs por hora</option>
