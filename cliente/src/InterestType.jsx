ebddf1b1cd173ab61ecf33ffb9b7f619e2f0f73e
lesly <lesly@gmail.com>
2024-03-27 19:18:39 -0400

    Mejorar front de perfil de amigos

---
+++ cliente/src/InterestType.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function InterestType({ interest }) {
+    return (
+        <div style={{ border: '2px solid black', padding: '5px', margin: '5px', display: 'inline-block' }}>
+            <p style={{ margin: '0' }}>{interest}</p>
+        </div>
+    );
+}
+
+export default InterestType;
