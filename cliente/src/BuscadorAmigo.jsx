1f5a1712559213be9b75a69ac379863dad8e087b
Emanuel Peredo <emanuelvpc06@gmail.com>
2024-03-27 15:09:33 -0400

    Se agregaron mas filtros en el frontend y un boton de Guardar

--- cliente/src/BuscadorAmigo.jsx
+++ cliente/src/BuscadorAmigo.jsx
@@ -2,9 +2,12 @@ import Buscador from "./Componentes/Buscador";
 import Navbar from "./Componentes/Navbar";
 import React, { useState } from 'react';
 import ComboBox from './Componentes/ComboBox';
+import Slider from './Componentes/ControldeslizanteEdad';
+import CheckboxGroup from './Componentes/CheckboxGroupIntereses';
+import BotonGuardar from './Componentes/BotonGuardar';
 
 function RegistrarDatosAmigo() {
-
+    //Arreglo y funciones para el CheckBox
      const optionsCiudades = [
       { label: 'Cochabamba', value: 'option1' },
       { label: 'La Paz', value: 'option2' },
@@ -26,9 +29,41 @@ function RegistrarDatosAmigo() {
      const handleComboBoxChangeGeneros = (event) => {
      setSelectedOptionGeneros(event.target.value);
      };
+    //Arreglo y funciones para el Slide Edad
+    const [sliderValue, setSliderValue] = useState(50);
 
-
+    const handleSliderChange = (event) => {
+    setSliderValue(event.target.value);
+    };
+    //Arreglo y Funciones para los checkbox de Interes
+    const options = [
+      { label: 'Musica', value: 'option1' },
+      { label: 'Compras', value: 'option2' },
+      { label: 'Deportes', value: 'option3' },
+      { label: 'Comida', value: 'option4' },
+      { label: 'Jardineria', value: 'option5' },
+      { label: 'videoJuegos', value: 'option6' },
+      { label: 'turismo', value: 'option7' },
+      { label: 'Fiestas', value: 'option8' },
+      { label: 'Cocinar', value: 'option9' },
+      { label: 'Comic', value: 'option10' },
+      { label: 'Libros', value: 'option11' },
+      { label: 'Bailar', value: 'option12' },
+      { label: 'idiomas', value: 'option13' },
+      { label: 'Cine', value: 'option14' },
+      // Agrega más opciones según sea necesario
+    ];
   
+    const [selectedOptions, setSelectedOptions] = useState([]);
+  
+    const handleCheckboxChange = (event) => {
+      const value = event.target.value;
+      if (selectedOptions.includes(value)) {
+        setSelectedOptions(selectedOptions.filter(option => option !== value));
+      } else {
+        setSelectedOptions([...selectedOptions, value]);
+      }
+    };
     return (
       <div>
         <Navbar/>
@@ -49,7 +84,19 @@ function RegistrarDatosAmigo() {
         selectedValue={selectedOptionGeneros}
         onChange={handleComboBoxChangeGeneros}
         />
-        
+        <Slider
+        label="Selecciona Edad"
+        min={18}
+        max={90}
+        value={sliderValue}
+        onChange={handleSliderChange}
+        />
+        <CheckboxGroup
+        options={options}
+        selectedOptions={selectedOptions}
+        onChange={handleCheckboxChange}
+        />
+        <BotonGuardar />
       
       </div>
     );
