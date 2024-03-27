7376263c73b2028022cf59887c51ffefe0bdf38b
Emanuel Peredo <emanuelvpc06@gmail.com>
2024-03-26 21:23:00 -0400

    Se agrego color a Navbar y cambio de letra a Helvetica

--- cliente/src/Componentes/Navbar.jsx
+++ cliente/src/Componentes/Navbar.jsx
@@ -2,20 +2,20 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-
+import './Navbar.css';
 function BasicExample() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="custom-navbar">
       <Container>
-        <Navbar.Brand href="#home">Amigo Rentable</Navbar.Brand>
+        <Navbar.Brand href="#home" className="brand-text">Amigo Rentable</Navbar.Brand> 
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">IniciarSesion</Nav.Link>
-            <NavDropdown title="Registro" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Registro Cliente</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Registro Amigo</NavDropdown.Item>
+            <Nav.Link href="#home" className="nav-link">Home</Nav.Link> 
+            <Nav.Link href="#link" className="nav-link">IniciarSesion</Nav.Link> 
+            <NavDropdown title="Registro" id="basic-nav-dropdown" className="nav-dropdown"> 
+              <NavDropdown.Item href="#action/3.1" className="dropdown-item">Registro Cliente</NavDropdown.Item> 
+              <NavDropdown.Item href="#action/3.2" className="dropdown-item">Registro Amigo</NavDropdown.Item> 
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
