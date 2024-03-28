0a9bd43975a659cef1b9f25f6e8ea166433fb94b
samitomonty <samitomonti@gmail.com>
2024-03-27 21:17:31 -0400

    mejore el fronted

--- cliente/src/PerfilAmigo.jsx
+++ cliente/src/PerfilAmigo.jsx
@@ -1,20 +1,47 @@
 import Button from "react-bootstrap/esm/Button";
+import Navbar from "./Componentes/Navbar";
+import './PerfilAmigo.css';
+import InterestType from "./InterestType";
+import React from 'react';
+
+
 
 function PerfilAmigo() {
-    return (
-      <div>
-      <h1>Perfil de PerfilAmigo</h1>
-     <img src =""></img>
-     <img src =""></img>
-     <img src =""></img>
-     <img src =""></img>
-     <Button>Volver </Button>
-     <Button>Alquilar Amigo</Button>
-      <h2>Intereses:</h2>
-
-
-      </div>
-    );
-  }
+  const profile = {
+    name: "Denis Pinto Paredes",
+    registration: "Registro realizado en Febrero del 2022 para Bolivia",
+    price: "100 BS/Hora",
+    interests: ["Cine", "Baile", "Natación"],
+    description: "Esta es una descripción del usuario."
+};
+return (
+  <div>
+    <Navbar/>
   
-  export default PerfilAmigo;
+  <div style={{ display: 'flex', alignItems: 'center' }}>
+    
+      <div style={{ border: '2px solid black', padding: '400px', marginRight: '20px' }}>
+          {/* Aquí colocarías la imagen del perfil */}
+      </div>
+      <div>
+      
+          <h1>Perfil</h1>
+          <p>Nombre: {profile.name}</p>
+          <p>Registro: {profile.registration}</p>
+          <p>Precio: {profile.price}</p>
+          <div>
+              <strong>Intereses:</strong>
+              <div style={{ flexDirection: 'row' }}>
+                  {profile.interests.map((interest, index) => (
+                      <InterestType key={index} interest={interest} />
+                  ))}
+              </div>
+          </div>
+          <p>Descripción: {profile.description}</p>
+      </div>
+  </div>
+  </div>
+);
+}
+
+export default PerfilAmigo;
