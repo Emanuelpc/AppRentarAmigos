75e39a43864019766884dbe573292ae5e08b426c
Amadeo Siles <amadeosilesdx2@gmail.com>
2024-03-29 20:29:40 -0400

    Avance Frontend En RegistrarHorarioAmigo ComboBox Turnos

--- cliente/src/RegistrarHorarioAmigo.jsx
+++ cliente/src/RegistrarHorarioAmigo.jsx
@@ -31,16 +31,81 @@ function RegistrarHorarioAmigo() {
 
             <input type="checkbox" id="sabado" name="sabado" value="sabado"/>
             <label for="sabado"> Sabado</label>
-
-      <h1>Esta es la página de RegistrarHorarioAmigo</h1>
-      <h1>Hola Mundo!!!</h1>
-      
- Jhonn
-
+    
             <input type="checkbox" id="domingo" name="domingo" value="domingo"/>
             <label for="domingo"> Domingo</label>
+
+            <br></br>
+            <br></br> 
+
+            <h3>Seleccionar Turno</h3>
+            <div className = "Turnos">
+                  <select name="Lunes" id="1">
+                    <option selected>Turno</option>
+                    <option value="1">Mañana</option>
+                    <option value="2">Tarde</option>
+                    <option value="3">Noche</option>
+                  </select>
+    
+                  <select name="Martes" id="2">
+                  <option selected>Turno</option>
+                    <option value="1">Mañana</option>
+                    <option value="2">Tarde</option>
+                    <option value="3">Noche</option>
+                  </select>
+
+                  <select name="Miercoles" id="3">
+                  <option selected>Turno</option>
+                    <option value="1">Mañana</option>
+                    <option value="2">Tarde</option>
+                    <option value="3">Noche</option>
+                  </select>
+
+                  <select name="Jueves" id="4">
+                  <option selected>Turno</option>
+                    <option value="1">Mañana</option>
+                    <option value="2">Tarde</option>
+                    <option value="3">Noche</option>
+                  </select>
+
+                  <select name="Viernes" id="5">
+                  <option selected>Turno</option>
+                    <option value="1">Mañana</option>
+                    <option value="2">Tarde</option>
+                    <option value="3">Noche</option>
+                  </select>
+
+                  <select name="Sabado" id="6">
+                  <option selected>Turno</option>
+                    <option value="1">Mañana</option>
+                    <option value="2">Tarde</option>
+                    <option value="3">Noche</option>
+                  </select>
+
+                  <select name="Domingo" id="7">
+                  <option selected>Turno</option>
+                    <option value="1">Mañana</option>
+                    <option value="2">Tarde</option>
+                    <option value="3">Noche</option>
+                  </select>
+            </div>
             </form>
+            <br></br> 
+              <div className="Horarios">
+                  <h5>Mañana (5:00 AM - 11:00 AM)</h5>
+                  <h5>Tarde (12:00 PM - 8:00 PM)</h5>
+                  <h5>Noche (9:00 AM - 1:00 AM)</h5> 
+              </div>
+
           </div>
+
+          <div>
+            <br></br>
+              <h2>Previsualización del Horario </h2>
+            
+          </div>
+
+
       </div>
     );
   }
