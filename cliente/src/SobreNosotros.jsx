b9143ee64d5e38dd2525499a6010ff1e39d1ca95
Emanuel Peredo <emanuelvpc06@gmail.com>
2024-04-01 16:47:49 -0400

    Fix Emanuel

---
+++ cliente/src/SobreNosotros.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function SobreNosotros() {
+    return (
+        <div>
+            <h1>Esta es la página sobre nosotros</h1>
+            <h1>SI es la Pagina alquilado sobre mi</h1>
+        </div>
+    )
+}
+
+export default SobreNosotros
