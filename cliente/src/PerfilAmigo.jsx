185562d4abc691b73cac8629e33cf98ffa63f400
lesly <202009997@est,umss.edu>
2024-04-01 23:37:40 -0400

    responsive añadido

--- cliente/src/PerfilAmigo.jsx
+++ cliente/src/PerfilAmigo.jsx
@@ -1,74 +1,114 @@
-import Navbar from "./Componentes/Navbar";
-import InterestType from "./InterestType";
-import './PerfilAmigo.css';
-import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { Button } from 'react-bootstrap'; // Importa Button desde react-bootstrap
-import * as bootstrap from 'bootstrap'; // Importa todo de bootstrap
+import React, { useState } from 'react';
+import Button from "react-bootstrap/esm/Button";
+import Toast from "react-bootstrap/esm/Toast";
+import ToastContainer from 'react-bootstrap/ToastContainer';
+import Navbar from "./Componentes/Navbar"
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Stack from 'react-bootstrap/Stack';
+
 
 function PerfilAmigo() {
-  const profile = {
-    name: "Denis Pinto Paredes",
-    registration: "Registro realizado en Febrero del 2022 para Bolivia",
-    price: "100 BS/Hora",
-    interests: ["Cine", "Baile", "Natación"],
-    description: "Esta es una descripción del usuario."
-  };
+  const [showA, setShow] = useState(false);
 
-  useEffect(() => {
-    const carousel = document.querySelector('#carouselExample');
-    const carouselInstance = new bootstrap.Carousel(carousel);
-  }, []); // Se ejecuta solo una vez después de que el componente se monta
+  const toggleShow = () => setShow(!showA);
+  const showToast = () => setShow(true);
+    return (
+      <div>
+        <Navbar/>
+          <Row className='p-3'>
+            <h1>Perfil de Amigo</h1>
+          </Row>
+          <Row className='p-3'>
+            <Col>{/* Aquí va el carrusel de imagenes */}
+              <div>
+                <Image className='p-3' width={350} height={400} color='gray' src="cliente/public/logo512.png" rounded /> 
+                <div>
+                  <Image className='p-3' width={171} height={180} color='gray' src="cliente/public/logo512.png" rounded /> 
+                  <Image className='p-3' width={171} height={180} color='gray' src="cliente/public/logo512.png" rounded /> 
+                  <Image className='p-3' width={171} height={180} color='gray' src="cliente/public/logo512.png" rounded /> 
+                </div> 
+              </div>
+              <div 
+                className="mt-3 p-2 d-flex justify-content-center" >
+                <div className="col-6 d-flex justify-content-end">
+                  <Button variant="secondary" size='md' href="/">Volver</Button>
+                </div>
+              </div>
+            </Col>
+            <Col>
+              <Row> {/* Aquí va la información del amigo */}
+                <div style={{ textAlign: 'left' }}>
+                  <h3>Nombre de Amigo</h3> 
+                </div>
+                <div style={{ textAlign: 'left' }}>
+                  <h4>Valorado con</h4> 
+                </div>
+                <div style={{ textAlign: 'left' }}>
+                  <h4>Registro realisado en $MES del $AÑO para $PAIS</h4> 
+                </div>
+                <div style={{ textAlign: 'left' }} className='py-2'>
+                  <h2>Precio Bs/Hora</h2> 
+                </div>
+              </Row>
+              <Row>{/* Aquí van los Intereses del Amigo*/}
+                <div className='py-2'>
+                 <div>
+                    <h2 style={{ textAlign: 'left' }}>Intereses:</h2> 
+                  </div>
+                 <div>
+                    <Stack direction="horizontal" gap={3}>
+                        <div className="p-2">First item</div>
+                        <div className="p-2">Second item</div>
+                        <div className="p-2">Third item</div>
+                        <div className="p-2">Fourth item</div>
+                    </Stack>
+                  </div>
+                 <div>
+                    <Stack direction="horizontal" gap={3}>
+                        <div className="p-2">First item</div>
+                        <div className="p-2">Second item</div>
+                        <div className="p-2">Third item</div>
+                        <div className="p-2">Fourth item</div>
+                    </Stack>
+                  </div>
+                </div>
+              </Row>
+              <Row className='m-4'>
+                <div className=" rounded border" style={{ height: '20vh', margin: '20px' }}>
+                  <p>Aquí va la descripción...</p>
+                </div>
+              </Row>
+              <div className="p-2 d-flex justify-content-center" >
+                <div className="col-6 d-flex justify-content-start">
+                  <Button variant="secondary" size='md' onClick={showToast}>Alquilar Amigo</Button>
+                </div>
+              </div>
+            </Col>
+          </Row>
+          <Row className='p-3 justify-content-center'>
+            <Col xs="auto">
+            </Col>
+            <Col xs="auto">
+            </Col>
 
-  return (
-    <div>
-      <Navbar />
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-        <div id="carouselExample" className="carousel slide" data-bs-ride="carousel"style={{ marginTop:'90px' }}>
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              <img src="https://img.freepik.com/fotos-premium/guapo-hombre-latino-sonriente-elegante-camiseta-blanca-compras-telefonos-moviles-linea_695242-226.jpg" className="d-block w-100" alt="..." />
-            </div>
-            <div className="carousel-item">
-              <img src="https://thumbs.dreamstime.com/b/hombre-cauc%C3%A1sico-hermoso-con-la-barba-76425603.jpg" className="d-block w-100" alt="..." />
-            </div>
-            <div className="carousel-item">
-              <img src="https://www.dzoom.org.es/wp-content/uploads/2019/06/fotografia-paisaje-consejos-12.jpg" className="d-block w-100" alt="..." />
-            </div>
-          </div>
-          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="visually-hidden">Next</span>
-          </button>
-        </div>
-        <div className="perfil">
-          <h1 id="titulo">Perfil</h1>
-          <p>Nombre: {profile.name}</p>
-          <p>Registro: {profile.registration}</p>
-          <p>Precio: {profile.price}</p>
-          <div>
-            <strong>Intereses:</strong>
-            <div style={{ flexDirection: 'row' }}>
-              {profile.interests.map((interest, index) => (
-                <InterestType key={index} interest={interest} />
-              ))}
-            </div>
-          </div>
-          <p>Descripción: {profile.description}</p>
-          <div className="botones">
-            <Link to="/BuscadorAmigo" style={{ textDecoration: 'none' }}>
-              <button className="boton-buscar-amigos">Volver</button>
-            </Link>
-            <Button variant="success">Rentar Amigo</Button>
-          </div>
-        </div>
+          </Row>
+            <Row>
+              <ToastContainer className="p-3" position="bottom-end">
+                <Toast onClose={toggleShow} show={showA}>
+                  <Toast.Header>
+                    <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+                    <strong className="me-auto">Correcto</strong>
+                    <small>Alquier exitoso</small>
+                  </Toast.Header>
+                  <Toast.Body>Has alquilado un amigo!!</Toast.Body>
+                </Toast>
+              </ToastContainer>
+            </Row>
+        
       </div>
-    </div>
-  );
-}
-
-export default PerfilAmigo;
+    );
+  }
+  
+  export default PerfilAmigo;
