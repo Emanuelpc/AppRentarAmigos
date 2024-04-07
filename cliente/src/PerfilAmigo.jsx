17703a7e25bb543543716c48960b98788f181436
Amadeo <amadeosilesdx2@gmail.com>
2024-04-07 18:21:42 -0400

    Diseño Frontend Visualizar Perfil

--- cliente/src/PerfilAmigo.jsx
+++ cliente/src/PerfilAmigo.jsx
@@ -1,127 +1,84 @@
+import React from 'react';
+import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCardImage, MDBBtn, MDBTypography } from 'mdb-react-ui-kit';
 import Navbar from "./Componentes/Navbar";
-import InterestType from "./InterestType";
-import "./PerfilAmigo.css";
-import React, { useState, useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
-import { Button } from "react-bootstrap";
-import * as bootstrap from "bootstrap";
-import Axios from "axios";
 
-function PerfilAmigo() {
-  //const [amigos, setamigos] = useState([]);
-  const location = useLocation();
-  const { nombre, apellido, descripcion, imagenUrl, genero, edad } =
-    location.state?.data || {};
-
-  const profile = {
-    name: nombre,
-    apellido: apellido,
-    edad: edad,
-    genero: genero,
-    price: "100 BS/Hora",
-    interests: ["Cine", "Baile", "Natación"],
-    description: descripcion,
-  };
-  //const getAmigo = () => {
-  // Axios.get("http://localhost:3001/amigos").then((response) => {
-  //  setamigos(response.data);
-  //});
-  //}
-
-  useEffect(() => {
-    const carousel = document.querySelector("#carouselExample");
-    const carouselInstance = new bootstrap.Carousel(carousel);
-    //getAmigo();
-  }, []); // Se ejecuta solo una vez después de que el componente se monta
-
+export default function PerfilAmigo() {
   return (
-    <div>
-      <Navbar />
-      <div className="d-flex align-items-center">
-        <div
-          id="carouselExample"
-          className="carousel slide container"
-          data-bs-ride="carousel"
-          style={{ marginTop: "90px" }}
-        >
-          <div className="carousel-inner ps-5">
-            <div className="carousel-item active">
-              <img
-                src="https://img.freepik.com/fotos-premium/guapo-hombre-latino-sonriente-elegante-camiseta-blanca-compras-telefonos-moviles-linea_695242-226.jpg"
-                className="d-block w-100"
-                alt="..."
-              />
-            </div>
-            <div className="carousel-item">
-              <img
-                src="https://thumbs.dreamstime.com/b/hombre-cauc%C3%A1sico-hermoso-con-la-barba-76425603.jpg"
-                className="d-block w-100"
-                alt="..."
-              />
-            </div>
-            <div className="carousel-item">
-              <img
-                src="https://www.dzoom.org.es/wp-content/uploads/2019/06/fotografia-paisaje-consejos-12.jpg"
-                className="d-block w-100"
-                alt="..."
-              />
-            </div>
-          </div>
-          <button
-            className="carousel-control-prev"
-            type="button"
-            data-bs-target="#carouselExample"
-            data-bs-slide="prev"
-          >
-            <span
-              className="carousel-control-prev-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button
-            className="carousel-control-next"
-            type="button"
-            data-bs-target="#carouselExample"
-            data-bs-slide="next"
-          >
-            <span
-              className="carousel-control-next-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Next</span>
-          </button>
+    <div className="gradient-custom-2" style={{ backgroundColor: '#536471' }}>
+        <div>
+        <Navbar/>
         </div>
-        <div className="perfil d-flex flex-column rounded-5 mt-5 container py-3">
-          <h1 id="titulo mx-auto">Perfil</h1>
-          <div className="text-start px-5">
-            <p>Nombre:{profile.name + " " + profile.apellido}</p>
-            <p>Edad:{profile.edad}</p>
-            <p>genero:{profile.genero}</p>
-            <p>Registro: {profile.registration}</p> 
-            <p>Precio: {profile.price}</p>
-          </div>
-          <div className="p-4">
-            <strong>Intereses:</strong>
-            <div style={{ flexDirection: "row" }}>
-              {profile.interests.map((interest, index) => (
-                <InterestType key={index} interest={interest} />
-              ))}
-            </div>
-          </div>
-          <div className="p-4">
-            <p>Descripcion:{profile.description}</p>
-          </div>
-          <div className="d-flex justify-content-around">
-            <Link to="/BuscadorAmigo" style={{ textDecoration: "none" }}>
-              <button className="btn btn-primary">Volver</button>
-            </Link>
-            <Button variant="success">Rentar Amigo</Button>
-          </div>
-        </div>
-      </div>
+      <MDBContainer className="py-5 h-100">
+        <MDBRow className="justify-content-center align-items-center h-100">
+          <MDBCol lg="9" xl="7">
+            <MDBCard>
+              <div className="rounded-top text-white d-flex flex-row" style={{ backgroundColor: '#000', height: '200px' }}>
+                <div className="ms-4 mt-5 d-flex flex-column" style={{ width: '150px' }}>
+                  <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"
+                    alt="Generic placeholder image" className="mt-4 mb-2 img-thumbnail" fluid style={{ width: '150px', zIndex: '1' }} />
+                  <MDBBtn outline color="dark" style={{height: '36px', overflow: 'visible'}}>
+                    Edit profile
+                  </MDBBtn>
+                </div>
+                <div className="ms-3" style={{ marginTop: '130px' }}>
+                  <MDBTypography tag="h5">Andy Horwitz</MDBTypography>
+                  <MDBCardText>New York</MDBCardText>
+                </div>
+              </div>
+              <div className="p-4 text-black" style={{ backgroundColor: '#f8f9fa' }}>
+                <div className="d-flex justify-content-end text-center py-1">
+                  <div>
+                    <MDBCardText className="mb-1 h5">253</MDBCardText>
+                    <MDBCardText className="small text-muted mb-0">Photos</MDBCardText>
+                  </div>
+                  <div className="px-3">
+                    <MDBCardText className="mb-1 h5">1026</MDBCardText>
+                    <MDBCardText className="small text-muted mb-0">Followers</MDBCardText>
+                  </div>
+                  <div>
+                    <MDBCardText className="mb-1 h5">478</MDBCardText>
+                    <MDBCardText className="small text-muted mb-0">Following</MDBCardText>
+                  </div>
+                </div>
+              </div>
+              <MDBCardBody className="text-black p-4">
+                <div className="mb-5">
+                  <p className="lead fw-normal mb-1">About</p>
+                  <div className="p-4" style={{ backgroundColor: '#f8f9fa' }}>
+                    <MDBCardText className="font-italic mb-1">Web Developer</MDBCardText>
+                    <MDBCardText className="font-italic mb-1">Lives in New York</MDBCardText>
+                    <MDBCardText className="font-italic mb-0">Photographer</MDBCardText>
+                  </div>
+                </div>
+                <div className="d-flex justify-content-between align-items-center mb-4">
+                  <MDBCardText className="lead fw-normal mb-0">Recent photos</MDBCardText>
+                  <MDBCardText className="mb-0"><a href="#!" className="text-muted">Show all</a></MDBCardText>
+                </div>
+                <MDBRow>
+                  <MDBCol className="mb-2">
+                    <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(112).webp"
+                      alt="image 1" className="w-100 rounded-3" />
+                  </MDBCol>
+                  <MDBCol className="mb-2">
+                    <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(107).webp"
+                      alt="image 1" className="w-100 rounded-3" />
+                  </MDBCol>
+                </MDBRow>
+                <MDBRow className="g-2">
+                  <MDBCol className="mb-2">
+                    <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(108).webp"
+                      alt="image 1" className="w-100 rounded-3" />
+                  </MDBCol>
+                  <MDBCol className="mb-2">
+                    <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(114).webp"
+                      alt="image 1" className="w-100 rounded-3" />
+                  </MDBCol>
+                </MDBRow>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
     </div>
   );
 }
-
-export default PerfilAmigo;
