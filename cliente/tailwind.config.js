51bb5b5c3e127e33c01f1a1057b1ebc834d38afd
lesly <202009997@est,umss.edu>
2024-04-26 12:05:01 -0400

    Cambios en Home, agregado de color y estructura, botones funcionales

--- cliente/tailwind.config.js
+++ cliente/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+          'celeste':'#ECF6FF'
+      }
+    },
   },
   plugins: [],
 }
